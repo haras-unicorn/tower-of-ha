3867a1adabd4c8eaaada5f00679bad27ac32fe76
haras-unicorn <github@haras-unicorn.net>
2026-06-25 11:11:52 +0200

    wip

--- src/modules/forgejo/test.nix
+++ src/modules/forgejo/test.nix
@@ -19,6 +19,32 @@
             toh.services.maddy.enable = true;
             toh.services.authelia.enable = true;
             toh.services.forgejo.enable = true;
+          };
+        };
+
+        toh.test.commands.perNodeInCluster.node = [
+          ''command_node.wait_until_succeeds("systemctl is-active forgejo.service", timeout=600)''
+          ''command_node.wait_until_succeeds("curl -f http://forgejo.service.toh/api/healthz", timeout=300)''
+        ];
+      };
+
+      checks.test-services-forgejo-runner = pkgs.tohPackages.testers.runToHTest {
+        name = "services-forgejo";
+
+        toh.test.clusters.node = {
+          amount = 3;
+          module = {
+            toh.services.etcd.enable = true;
+            toh.services.coredns.enable = true;
+            toh.services.haproxy.enable = true;
+            toh.services.patroni.enable = true;
+            toh.services.cephfs.enable = true;
+            toh.services.garage.enable = true;
+            toh.services.valkey.enable = true;
+            toh.services.lldap.enable = true;
+            toh.services.maddy.enable = true;
+            toh.services.authelia.enable = true;
+            toh.services.forgejo.enable = true;
             toh.services.forgejo.runner.enable = true;
           };
         };
